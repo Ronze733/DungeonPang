9699120f2e5742e3fe57bb6e17be1b26be37a9c5
Scodeclearer <132992563+Scodeclearer@users.noreply.github.com>
2023-07-18 11:21:46 +0900

    change

--- Assets/Scripts/Monster/Monster.cs
+++ Assets/Scripts/Monster/Monster.cs
@@ -9,9 +9,7 @@ public class Monster : MonoBehaviour
     [SerializeField]
     private GameObject _player;
 
- 
     private HealthPoint _heathPoint;
-
 
     [SerializeField]
     private float _attackRange = 1f;
@@ -19,33 +17,33 @@ public class Monster : MonoBehaviour
     public float _speed;
     private Vector2 _dir;
 
-    private bool _isDead = false;
+    private static bool _isDead = false; // 정적 변수로 변경
 
     private void Start()
     {
-       _heathPoint = this.GetComponent<HealthPoint>();
+        _heathPoint = GetComponent<HealthPoint>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(_isDead) return;
+        if (_isDead) return;
 
         if (_heathPoint.HP == 0 && !_isDead)
         {
             Dead();
             _isDead = true;
-            StartCoroutine(DestroyAfterDelay(2f));
+            StartCoroutine(DestroyAfterDelay(2f)); // 2초 뒤에 사라지도록 설정
             return;
         }
 
-        _dir = _player.transform.position - this.transform.position;
+        _dir = _player.transform.position - transform.position;
         if (_player != null)
         {
             if (_dir.magnitude < _attackRange)
-                this.gameObject.GetComponent<Animator>().SetBool("CanAttack", true);
+                GetComponent<Animator>().SetBool("CanAttack", true);
             else
-                this.gameObject.GetComponent<Animator>().SetBool("CanAttack", false);
+                GetComponent<Animator>().SetBool("CanAttack", false);
         }
 
         if (_dir.x < 0)
@@ -61,27 +59,21 @@ public class Monster : MonoBehaviour
 
         transform.Translate(_dir.normalized * speed * Time.deltaTime);
 
-        this.gameObject.GetComponent<Animator>().SetFloat("Speed", speed);
-
-        
-
-
+        GetComponent<Animator>().SetFloat("Speed", speed);
     }
 
     private void Dead()
     {
-        this.gameObject.GetComponent<Animator>().SetTrigger("Dead");
+        GetComponent<Animator>().SetTrigger("Dead");
     }
 
     private void Turn(int direction)
     {
-        var scale = this.gameObject.transform.localScale;
-
+        var scale = transform.localScale;
         scale.x = Mathf.Sign(direction) * Mathf.Abs(scale.x);
-
-        gameObject.transform.localScale = scale;
-
+        transform.localScale = scale;
     }
+
     private IEnumerator DestroyAfterDelay(float delay)
     {
         yield return new WaitForSeconds(delay);
