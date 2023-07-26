a07259f4eee2eff7bee624c92db1e9521ac446fb
Jaemin Hwang <forttrl20@gmail.com>
2023-07-26 13:03:05 +0900

    Star Attack System update

--- Assets/Scripts/CharacterScrips/Weapon/StarProjectile.cs
+++ Assets/Scripts/CharacterScrips/Weapon/StarProjectile.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
 
 public class StarProjectile : MonoBehaviour
 {
@@ -30,16 +32,53 @@ public class StarProjectile : MonoBehaviour
         set { _speed = value; }
     }
 
+    private Vector3 _movement;
+
+    private bool _isXCollision = false;
+    private bool _isYCollision = false;
+    private float _collisionXTime = 0f;
+    private float _collisionYTime = 0f;
+
     private void Awake()
     {
         if(_setSpeed != 0f)
             _speed = _setSpeed;
     }
 
+    private void Start()
+    {
+        _movement = this.transform.right * _speed;
+        Vector3 pos = this.transform.position;
+        pos.z = -5f;
+        this.transform.position = pos;
+    }
+
     private void Update()
     {
-        Transform star = this.transform;
-        star.Translate(star.right * _speed * Time.deltaTime, Space.World);
+
+        Vector3 position = this.transform.position;
+        _movement.z = 0f;
+        position += _movement * Time.deltaTime;
+
+        this.transform.position = position;
+
+        if(_isXCollision)
+            _collisionXTime += Time.deltaTime;
+
+        if(_isYCollision)
+            _collisionYTime += Time.deltaTime;
+
+        if(_collisionXTime > 1f)
+        {
+            _collisionXTime = 0.0f;
+            _isXCollision = false;
+        }
+
+        if (_collisionYTime > 1f)
+        {
+            _collisionYTime = 0.0f;
+            _isYCollision = false;
+        }
     }
 
 
@@ -61,6 +100,17 @@ public class StarProjectile : MonoBehaviour
                     monsterCollider.GetComponent<HealthPoint>().MonsterHit(_damage);
                 }
             }
+        }
+        
+        if (collision.tag == "XWall" && !_isXCollision)
+        {
+            _movement.x *= -1;
+            _isXCollision = true;
+        }
+        else if (collision.tag == "YWall" && !_isYCollision)
+        {
+            _movement.y *= -1;
+            _isYCollision=true;
         }
     }
 }
