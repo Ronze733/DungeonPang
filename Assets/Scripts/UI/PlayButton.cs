99223934880ba52f483ecdd347ab1377ffc700bd
Jaemin Hwang <forttrl20@gmail.com>
2023-08-04 10:40:31 +0900

    ButtonUpdate

--- Assets/Scripts/UI/PlayButton.cs
+++ Assets/Scripts/UI/PlayButton.cs
@@ -4,24 +4,10 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PlayButton : MonoBehaviour
+public class PlayButton : ButtonController
 {
     private void Start()
     {
-        ChangeButtonText();
-    }
-
-    private void ChangeButtonText()
-    {
-        TMP_Text tmpText = GetComponentInChildren<TMP_Text>();
-        if (tmpText != null)
-            tmpText.text = "Play";
-        else
-            Debug.LogWarning("Tmp_text component not found as a child of the button");
-    }
-
-    public void LoadGameScene()
-    {
-        SceneManager.LoadScene("MainScene");
+        ChangeButtonText("Play");
     }
 }
